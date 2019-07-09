2c7f70030a9725df1643f199128d7f7bb1c6a792
root <root@imaas.incpm.weizmann.ac.il>
2019-07-09 14:47:14 +0300

    Add awk

--- clean-3-year-old-files-arg-test.sh
+++ clean-3-year-old-files-arg-test.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-#set -x dd
+#set -x
 while getopts f:e: option
 do
 case "${option}"
@@ -33,10 +33,17 @@ find -L $FOLDER* -maxdepth 0 -type d | while read wis; do
 							#echo "${wis}"
 							#du -hs "${wis}"
                                                         (COUNT=`find -L "${wis}" -type d -mtime -1095 -print -quit | wc -l`;if [ $COUNT -eq 0 ];then du -hs "${wis}";fi) &
+							pids[${i}]=$!
                                         fi
                                
 	done
 
+# wait for all pids
+for pid in ${pids[*]}; do
+    wait $pid
+done
 
+echo "All processes done!"
 
 #find -L $x* -maxdepth 0 -type d -print| xargs -0 | parallel -k "find -L {} -type d -mtime +1095 -and -not -mtime -1095 -print -quit"
+# grep -e '^[0-9]*[0-9]M'  bioinformatics-projects-3-years-8-7-2019.txt | sort -nr | awk '{total+=$1} END {print total}'
